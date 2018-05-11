d9ca79aef026ff123a7fb3ed9e936cfc16f2027f
Joshua Hoke <joshua.hoke@gmail.com>
2018-05-10 20:54:29 -0400

    Add form view, get the hang of Cypress

---
+++ src/views/Form.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Form binding test</h1>
+  </div>
+</template>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,22 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+  it('Visits the app root url', () => {    
     cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+
+  it('Visits the about page', () => {    
+    cy.get('[href="#/about"]')
+      .click()
+    cy.contains('h1', 'This is an about page')
+  })
+
+  it('Visits the form page', () => {
+    cy.get('[href="#/form"]')
+      .click()
+    cy.contains('h1', 'Form binding test')
   })
 })
